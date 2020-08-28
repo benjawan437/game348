3d30243d5e789bfd6fe7c945b4aa05fca5969b7f
benjawn497 <benjawn497@gmail.com>
2020-08-28 22:09:29 +0700

    u

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,125 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <game/>
-  </div>
-</template> 
+<div id="app">
+    <div class="can">
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <h1>{{go_name}}</h1>
+                <b-progress :value="go_hp" :max="g_max" class="bar" show-value variant="succecss"></b-progress>
+                <div class="back">
+                    <img :src="g_img" class="img-fluid image" :width="go_hp + 'px'" />
+                </div>
+            </div>
+            <div class="col-md-4 mt-5 col-sm-4">
+                <img src="./assets/a1.png" />
+            </div>
+            <div class="col-md-4 col-sm-4">
+                <h1>{{ev_name}}</h1>
+                <b-progress :value="ev_hp" :max="e_max" class="bar" show-value variant="succecss"></b-progress>
+                <div class="back">
+                    <img :src=" e_img" class="img-fluid image" :width="ev_hp + 'px'" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <game @Gname="gname" @Ghp="ghp" @Gimage="gimage" @Ename="ename" @Ehp="ehp" @Eimage="eimage" @Gten="gten" @Eten="eten" @Gwin="gwin" @Ewin="ewin"></game>
+        </div>
+    </div>
+</div>
+</template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import game from './components/game.vue'
-
+import game from "./components/game";
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    game,
-
-  },
-
-  data: function () {
-    return {
-      total: 0, //ผลรวมทั้งหมด ---> เอาไปแสดงผล{{}}
-      name: "",
-      code: "",
-      gpa: "",
-    };
-  },
-
-  methods: {
-    sum(counterValue) {
-      this.total += counterValue;
+    name: "App",
+    components: {
+        //HelloWorld,
+        game,
     },
-  },
+
+    data: function () {
+        return {
+            go_name: "",
+            ev_name: "",
+            g_max: 0,
+            e_max: 0,
+            go_hp: 0,
+            ev_hp: 0,
+            attack: 0,
+            g_img: "",
+            e_img: "",
+        };
+    },
+
+    methods: {
+        //Good side
+        gname(value) {
+            this.go_name = value;
+        },
+        ghp(value) {
+            this.go_hp = value;
+            this.g_max = value;
+        },
+        gimage(value) {
+            this.g_img = value;
+        },
+        //Evil side
+        ename(value) {
+            this.ev_name = value;
+        },
+        ehp(value) {
+            this.ev_hp = value;
+            this.e_max = value;
+        },
+        eimage(value) {
+            this.e_img = value;
+        },
+        //ATTACK Good side
+        gten(value) {
+            this.attack = value;
+            this.ev_hp -= this.attack;
+        },
+        gwin(value) {
+            this.attack = value;
+            this.ev_hp -= this.attack;
+        },
+        //ATTACK Evil side
+        eten(value) {
+            this.attack = value;
+            this.go_hp -= this.attack;
+        },
+        ewin(value) {
+            this.attack = value;
+            this.go_hp -= this.attack;
+        },
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #313030;
+}
+
+.can {
+    margin: auto;
+    width: 90%;
+    height: 600px;
+}
+
+.gvse {
+    margin-top: 20vw;
+}
+
+.back {
+    display: inline-block;
+    margin: 5px;
+    bottom: 0;
 }
 </style>
--- src/components/game.vue
+++ src/components/game.vue
@@ -1,105 +1,168 @@
 <template>
-  <div>
-
-    {{player[randomNO(1,4)-1].name}}<br><br>
+<div>
     <!--ได้ทั้งชื่อและ hp ออกมา-->
     <!--{{randomDamage(1,300)}} สุ่ม 1 - 300-->
-    <button v-on:click="$start" class="button-a" id="yellow">START</button>
-    <button v-on:click="$attack" class="button-a" id="purple">ATTACH</button>
-    <hr />
-    name = {{aPlayer.name}}
-    hp = {{aPlayer.hp}}
-    image = {{aPlayer.image}}
-    <hr />
-    name = {{bMonster.name}}
-    hp = {{bMonster.hp}}
-    image = {{bMonster.image}}
+    <button v-on:click="$start" class="button-a" id="start">START</button>
+    <button v-on:click="$attack" class="button-a" id="ten">ATTACH</button>
+    <button v-on:click="$attack" class="button-a" id="ten">SPECIAL ATTACH</button>
 
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      //เอาชื่อออกจาก object player[1].name
-      //เอาชื่อออกจาก object player[random].name
-      aPlayer: { name: "", hp: 0, image: "" },
-      bMonster: { name: "", hp: 0, image: "" },
-      attack: 0,
-      special: 0,
-      player: [
-        { name: "SM", hp: 160, image: "a" },
-        { name: "JYP", hp: 90, image: "b" },
-        { name: "YG", hp: 115, image: "c" },
-        { name: "BIGHIT", hp: 145, image: "d" },
-      ],
-      monster: [
-        { name: "Sasaengs", hp: 90, image: "x" },
-        { name: "Fanclub", hp: 110, image: "y" },
-        { name: "Dispatch", hp: 125, image: "z" },
-        { name: "Idol", hp: 150, image: "v" },
-      ],
-    };
-  },
-  methods: {
-    randomNO: function (min, max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min);
-    },
-    $start: function () {
-      //1.random Plaryer 1-3 -->ชื่อ ค่า hp รูป ฝากไว้ใน aPlary
-      this.aPlayer = this.player[this.randomNO(1, 4) - 1];
-      this.bMonster = this.monster[this.randomNO(1, 4) - 1];
-      //2.random Monster 1-3
-    },
-    $attack: function () {
-        // p -> mon
-        // p <- mon
-        //p <=0 ===ทำไง  you lose
-        //mon <==0 ทำไง you win
-        //End
+    data: function () {
+        return {
+            //เอาชื่อออกจาก object player[1].name
+            //เอาชื่อออกจาก object player[random].name
+            player: [{
+                    name: "Levi",
+                    hp: 260,
+                    image: require("../assets/1.png"),
+                },
+                {
+                    name: "Eren Titan",
+                    hp: 210,
+                    image: require("../assets/4.png"),
+                },
+                {
+                    name: "Armin",
+                    hp: 110,
+                    image: require("../assets/3.png"),
+                },
+                {
+                    name: "Mikasa",
+                    hp: 230,
+                    image: require("../assets/2.png"),
+                },
+                {
+                    name: "Jean",
+                    hp: 120,
+                    image: require("../assets/5.png"),
+                },
+            ],
 
-
+            monster: [{
+                    name: "Colossal Titan",
+                    hp: 210,
+                    image: require("../assets/11.png"),
+                },
+                {
+                    name: "Armored Titan",
+                    hp: 230,
+                    image: require("../assets/13.png"),
+                },
+                {
+                    name: "Beast Titan",
+                    hp: 255,
+                    image: require("../assets/14.png"),
+                },
+                {
+                    name: "Female Titan",
+                    hp: 190,
+                    image: require("../assets/12.png"),
+                },
+                {
+                    name: "Annie",
+                    hp: 120,
+                    image: require("../assets/15.png"),
+                },
+            ],
+        };
     },
-    $special: function () {},
-  },
+    methods: {
+        randomNO: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        $start: function () {
+            this.plaran = this.randomNO(1, 5) - 1;
+            this.monran = this.randomNO(1, 5) - 1;
+            //1.random Plaryer 1-3 -->ชื่อ ค่า hp รูป ฝากไว้ใน aPlary
+            this.nameP1 = this.player[this.plaran].name;
+            this.$emit("Gname", this.nameP1);
+            console.log("emit", this.nameP1);
+            this.hpp1 = this.player[this.plaran].hp;
+            this.$emit("Ghp", this.hpp1);
+            console.log("hp", this.hpp1);
+            this.imag1 = this.player[this.plaran].image;
+            this.$emit("Gimage", this.imag1);
+            console.log("ima", this.imag1);
+            //2.random Monster 1-3
+            this.nameM2 = this.monster[this.monran].name;
+            console.log("emit", this.nameM2);
+            this.$emit("Ename", this.nameM2);
+            this.hpM2 = this.monster[this.monran].hp;
+            this.$emit("Ehp", this.hpM2);
+            this.imagM2 = this.monster[this.monran].image;
+            this.$emit("Eimage", this.imagM2);
+        },
+        $attack: function () {
+            this.ten = this.randomNO(3, 10);
+            this.$emit("Gten", this.ten);
+            console.log("attack", this.nameM2);
+
+            this.ten = this.randomNO(3, 10);
+            this.$emit("Eten", this.ten);
+            console.log("atte", this.nameM2);
+
+        },
+        $special: function () {
+            this.sa = this.randomNO(10, 20);
+            this.$emit("Gwin", this.sa);
+            console.log("sattag", this.nameM2);
+
+            this.sa = this.randomNO(10, 20);
+            this.$emit("Ewin", this.sa);
+            console.log("satte", this.nameM2);
+
+        },
+    },
 };
 </script>
+
 <style scoped>
 .b {
-  font-size: 50px;
+    font-size: 50px;
 }
+
 .button-a {
-  background-color: rgb(80, 79, 79);
-  border: none;
-  padding: 5px 5px 5px 5px;
-  text-align: center;
-  display: inline-block;
-  margin: 4px 2px;
-  font-size: 50px;
-  cursor: pointer;
-  border-radius: 10px;
-  border: 0.5px solid white;
-  width: 300px;
+    background-color: rgba(49, 49, 49, 0.8);
+    border: none;
+    padding: 5px 5px 5px 5px;
+    text-align: center;
+    display: inline-block;
+    margin: 4px 2px;
+    font-size: 30px;
+    cursor: pointer;
+    border-radius: 10px;
+    border: 0.5px solid rgb(43, 43, 43);
+    width: 300px;
 }
+
 .button-a-font-dd {
-  background-color: rgb(250, 223, 103);
-  border: none;
-  color: #d32c2c;
-  padding: 5px 5px 5px 5px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 10px;
-  border: 0.5px solid #ffffff;
-  width: 300px;
+    background-color: rgb(250, 223, 103);
+    border: none;
+    color: #d32c2c;
+    padding: 5px 5px 5px 5px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 10px;
+    border: 0.5px solid #ffffff;
+    width: 300px;
 }
-button:hover#yellow {
-  color: #f70000;
+
+button:hover#num1 {
+    color: #76ff26;
 }
-button:hover#purple {
-  color: #1df700;
+
+button:hover#num2 {
+    color: #ff2b2b;
+}
+
+button:hover#num3 {
+    color: #fffc34;
 }
 </style>
